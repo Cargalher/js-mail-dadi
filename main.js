437e5a49210d90c8349013c57f00795e464f9058
Cargalher <cargalher@gmail.com>
2021-04-26 16:07:36 +0200

    exercise commented. making the exercise using the Array

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// *************** dice game NOT USING ARRAY ***************
+/*
 // random number_Computer
 var computerDice = Math.floor(Math.random() * 6) + 1;
 console.log("The computer rolls a "+ computerDice);
@@ -16,3 +18,8 @@ if (userDice > computerDice) {
     console.log("You roll a " + userDice + " and the computer rolls a " + computerDice + ". This is a Tie!!" );
     document.getElementById('dices').innerHTML= "You roll a " + userDice + " and the computer rolls a " + computerDice + ". This is a Tie!!" ;
 };
+*/
+
+
+// *************** dice game USING ARRAY ***************
+var diceRoll = [1, 2, 3, 4, 5, 6];
