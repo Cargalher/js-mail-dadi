17aa0d8ae5d406bf1529dae04a5404009aa8b021
Cargalher <cargalher@gmail.com>
2021-04-26 16:19:09 +0200

    dice game made with array method

--- main.js
+++ main.js
@@ -31,3 +31,14 @@ console.log("The computer rolls a "+ computerDice);
 // random number_User
 var userDice = diceRoll[Math.floor(Math.random() * 6)];
 console.log("The user rolls a "+ userDice);
+
+if (userDice > computerDice) {
+    console.log("You roll a " + userDice + " and the computer rolls a " + computerDice + ". You Win!!" );
+    document.getElementById('dices').innerHTML= "You roll a " + userDice + " and the computer rolls a " + computerDice + ". You Win!!" ;
+} else if (userDice < computerDice){
+    console.log("You roll a " + userDice + " and the computer rolls a " + computerDice + ". You Lose!!" );
+    document.getElementById('dices').innerHTML= "You roll a " + userDice + " and the computer rolls a " + computerDice + ". You Lose!!" ;
+} else if (userDice === computerDice){
+    console.log("You roll a " + userDice + " and the computer rolls a " + computerDice + ". This is a Tie!!" );
+    document.getElementById('dices').innerHTML= "You roll a " + userDice + " and the computer rolls a " + computerDice + ". This is a Tie!!" ;
+};
