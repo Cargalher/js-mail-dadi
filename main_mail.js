6c075ebd1c8e3b6c3f2670b1d35ec5d88a19c5c6
Cargalher <cargalher@gmail.com>
2021-04-26 16:39:04 +0200

    variable with email list_using array

---
+++ main_mail.js
@@ -0,0 +1,4 @@
+// emails list
+var emailList = ["chiara@gmail.com", "mario@gmail.com", "paola@gmail.com", "lorenzo@gmail.com", "carmen@yahoo.com", "francisco@yahoo.com", "lisa@yahoo.com", "federico@yahoo.com", "leonardo@yahoo.com" ];
+var yourEmail = prompt ("What is your email address?")
+console.log("What is your email address");
