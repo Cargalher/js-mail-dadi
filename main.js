f20ac3cc77792eb02e00663c904448f5d005487f
Cargalher <cargalher@gmail.com>
2021-04-26 15:58:49 +0200

    dice game made with conditional (without array)

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ var userDice = Math.floor(Math.random() * 6) + 1;
 console.log("The user rolls a "+ userDice);
 
 if (userDice > computerDice) {
-console.log("You roll a " + userDice + " the computer rolls a " + computerDice + ". You Win!!" );
+    console.log("You roll a " + userDice + " and the computer rolls a " + computerDice + ". You Win!!" );
+    document.getElementById('dices').innerHTML= "You roll a " + userDice + " and the computer rolls a " + computerDice + ". You Win!!" ;
 } else if (userDice < computerDice){
-    console.log("You roll a " + userDice + " the computer rolls a " + computerDice + ". You Lose!!" );
+    console.log("You roll a " + userDice + " and the computer rolls a " + computerDice + ". You Lose!!" );
+    document.getElementById('dices').innerHTML= "You roll a " + userDice + " and the computer rolls a " + computerDice + ". You Lose!!" ;
 } else if (userDice === computerDice){
-    console.log("You roll a " + userDice + " the computer rolls a " + computerDice + ". This is a Tie!!" );
+    console.log("You roll a " + userDice + " and the computer rolls a " + computerDice + ". This is a Tie!!" );
+    document.getElementById('dices').innerHTML= "You roll a " + userDice + " and the computer rolls a " + computerDice + ". This is a Tie!!" ;
 };
