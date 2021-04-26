e69e69bb2de48da714959cf40e15e3084c5a81f0
Cargalher <cargalher@gmail.com>
2021-04-26 15:49:51 +0200

    if -else used to roll dices_consolo log used

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 // random number_Computer
 var computerDice = Math.floor(Math.random() * 6) + 1;
 console.log("The computer rolls a "+ computerDice);
+
 // random number_User
 var userDice = Math.floor(Math.random() * 6) + 1;
 console.log("The user rolls a "+ userDice);
+
+if (userDice > computerDice) {
+console.log("You roll a " + userDice + " the computer rolls a " + computerDice + ". You Win!!" );
+} else if (userDice < computerDice){
+    console.log("You roll a " + userDice + " the computer rolls a " + computerDice + ". You Lose!!" );
+} else if (userDice === computerDice){
+    console.log("You roll a " + userDice + " the computer rolls a " + computerDice + ". This is a Tie!!" );
+};
