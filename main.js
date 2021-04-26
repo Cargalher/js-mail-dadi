6c5b567c9d5df54e79e520113fd615b77fd76691
Cargalher <cargalher@gmail.com>
2021-04-26 15:34:53 +0200

    Creating random numbers for computer and user

---
+++ main.js
@@ -0,0 +1,6 @@
+// random number_Computer
+var computerDice = Math.floor(Math.random() * 6) + 1;
+console.log("The computer rolls a "+ computerDice);
+// random number_User
+var userDice = Math.floor(Math.random() * 6) + 1;
+console.log("The user rolls a "+ userDice);
